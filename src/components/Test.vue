f28bb9f8aa28a99cf0a8bb50b6ff93d92f48287b
MaiXiaochai <maixiaochai@qq.com>
2020-06-23 22:58:01 +0800

    *）强制数据绑定

---
+++ src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Test 组件</h1>
+        <h3>强制数据绑定</h3>
+        <a v-bind:href="site">学习</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Test',
+        data() {
+            return {
+                site:'https://github.com/MaiXiaochai'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
