e98d8ac27967d5429568a773533979049467c5ed
MaiXiaochai <maixiaochai@qq.com>
2020-07-28 22:42:16 +0800

    *)->|条件渲染指令

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Test></Test>
-    <ComputedAndWatch />
-  </div>
+    <div id="app">
+        <ClassAndStyle />
+<!--        <img alt="Vue logo" src="./assets/logo.png">-->
+<!--        <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+<!--        <Test></Test>-->
+<!--        <ComputedAndWatch/>-->
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Test from './components/Test.vue'
-import ComputedAndWatch from './components/ComputedAndWatch.vue'
+    // import HelloWorld from './components/HelloWorld.vue'
+    // import Test from './components/Test.vue'
+    // import ComputedAndWatch from './components/ComputedAndWatch.vue'
+    import ClassAndStyle from "./components/ClassAndStyle";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Test,
-    ComputedAndWatch
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            // HelloWorld,
+            // Test,
+            // ComputedAndWatch,
+            ClassAndStyle
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/ClassAndStyle.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h3>class的使用</h3>
+        <div :class="oneClass">样式类使用字符串</div>
+        <div :class="{classOne: true, classTwo: true}">样式类使用对象</div>
+        <div :class="['classOne', 'classTwo']">样式类使用数组，方式一</div>
+        <div :class="[oneClass, twoClass]">样式类使用数组，方式二</div>
+        <div :class="[oneClass, 'classTwo']">样式类使用数组，方式三</div>
+        <br>
+        <hr>
+        <br>
+        <h3>style的使用</h3>
+        <div
+                style="width: 300px; height: 200px; margin: 10px auto;"
+                :style="{backgroundColor: bgColor, fontSize: fSize}"
+        >样式可以是字符串</div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ClassAndStyle",
+        data() {
+            return {
+                oneClass: 'classOne',
+                twoClass: 'classTwo',
+                bgColor: 'red',
+                fSize: '30px'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .classOne {
+        font-size: 20px;
+        color: red;
+    }
+
+    .classTwo {
+        width: 400px;
+        height: 200px;
+        background-color: deepskyblue;
+        margin: 10px auto;
+    }
+</style>
