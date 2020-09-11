ac11827f8e6ba9700b11860d8e80445a52264495
MaiXiaochai <maixiaochai@qq.com>
2020-09-11 14:20:08 +0800

    *)vuex 安装前准备

---
+++ src/views/VuexDemo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>VueX-Demo</p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "VuexDemo"
+    }
+</script>
+
+<style scoped>
+
+</style>
